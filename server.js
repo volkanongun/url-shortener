f6b2916e8cb84ab78a158164f14a793bae9e0dd3
Volkan Ongun <me@volkanongun.com>
2020-02-26 16:57:52 +0300

    project complete but not working

--- server.js
+++ server.js
@@ -1,15 +1,38 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const ShortUrl = require('./models/shortURL')
+
+mongoose.connect('mongodb://localhost/urlShortener', {
+	useNewUrlParser : true,
+	useUnifiedTopology: true
+})
 
 const app = express();
 
 app.set("view engine", "ejs")
+app.use(express.urlencoded({extended: false}))
 
-app.get("/", (req,res)=>{
-	res.render("index")
+app.get("/", async (req,res)=>{
+	const shortURLs = await ShortUrl.find();
+	res.render("index", {shortURLs : shortURLs});
 })
 
-app.post("/shortURLs", (req,res)=>{
-	
+app.post("/shortURLs", async (req,res)=>{
+	await ShortUrl.create({full : req.body.fullURL})
+	res.redirect('/')
+})
+
+app.get('/:shortURL', async (req,res) => {
+	const shortUrl = await ShortUrl.findOne({short : req.params.shortUrl})
+
+	if (shortUrl == null) {
+		return res.sendStatus(404)
+	}
+
+	shortUrl.clicks++
+	shortUrl.save()
+
+	res.redirect(shortUrl.full);
 })
 
 app.listen(process.env.PORT || 5000)
